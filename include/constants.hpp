4151ccff7ca7a4b334fddd4a349cdd07f0ed2bff
Lucas Newcomb <pantsbabyy@gmail.com>
2026-04-03 00:45:11 -0400

    Move to CMake

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,5 +1,3 @@
-#include "SFML/Graphics/Color.hpp"
-#include "SFML/System/Vector2.hpp"
 constexpr double SOFTENING = 1e-12;
 
 // Units
@@ -23,6 +21,6 @@ constexpr float PIXELS_PER_UNIT = 1.0 / L0;
 constexpr float MIN_BODY_RADIUS = 0.1;
 constexpr float BODY_RADIUS = 1.5;
 constexpr float MAX_BODY_RADIUS = 3.0;
-constexpr sf::Color BODY_COLOR = sf::Color::White;
-constexpr sf::Vector2f ORIGIN = {SCREEN_W / 2.0, SCREEN_H / 2.0};
+// constexpr sf::Color BODY_COLOR = sf::Color::White;
+// constexpr sf::Vector2f ORIGIN = {SCREEN_W / 2.0, SCREEN_H / 2.0};
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,19 @@
-#include <GL/gl.h>
-#include <stdio.h>
 #include <utility>
 #include <immintrin.h>
 
-#include "gfx/renderer.hpp"
 #include "constants.hpp"
 #include "sim_state.hpp"
 
-#define N 1e4
+#define N 1e3
+#define DT 0.01
+#define RUNS 5000
 
 int main() {
     SimState current(N, {1e2, 1e4}, {-400, 400});
     SimState next(N);
-    sf::ContextSettings settings;
-    settings.antiAliasingLevel = 8;
-    Renderer renderer(SCREEN_W, SCREEN_H, "N-Body", settings);
-    glPointSize(2.f);
-    sf::Clock clock;
 
-    while (renderer.window.isOpen()) {
-        while (const std::optional event = renderer.window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                renderer.window.close();
-            }
-        }
-        float dt = clock.restart().asSeconds();
-        if (!renderer.paused) {
-            step(current, next, 0.01);
-        }
-        renderer.window.clear(sf::Color::Black);
-        renderer.draw(current);
-        renderer.window.display();
+    for (int i = 0; i < RUNS; i++) {
+        step(current, next, DT);
+        printState(current, i);
     }
 }
