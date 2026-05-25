fe5caf81267acf29bacffe637441e61e25910a0d
Lucas Newcomb <pantsbabyy@gmail.com>
2026-05-24 23:34:50 -0400

    Finished solvers

--- include/solvers/direct_openmp.hpp
+++ include/solvers/direct_openmp.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "constants.hpp"
 #include "physics/ephemeris.hpp"
 
 namespace solvers {
 
 class DirectOpenMP {
 private:
-   double softening = 1e-12;
+   double softening = SOFTENING;
 public:
     void computeBounds(physics::Ephemeris &s);
     void computeAccel(physics::Ephemeris &s);
